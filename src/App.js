e6a97647ba85c6ea33438e20756d356d117bfc06
Aaron Merrick <aaronmerrick.alpha@gmail.com>
2021-10-17 12:38:30 -0500

    creating header prototype

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import logo from './assets/img/logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App mwi-background">
+      <div className="mwi-container">
+        <header className="mwi-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          
+          <button
+            className="contact-link"
+            href="contact"
+            rel="noopener noreferrer"
+          >
+            contact
+          </button>
+        </header>
+      </div>
     </div>
   );
 }

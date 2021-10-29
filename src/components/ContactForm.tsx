1c67a471bac502160874db7e2563a0e6ccc31dcc
Aaron Merrick <aaronmerrick.alpha@gmail.com>
2021-10-28 20:01:22 -0500

    Finishing styling the contact form, enhancing validation, and fixing a bug with the submit button

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -14,10 +14,14 @@ interface formState {
         email: string,
         message: string
     },
-    isValid: Boolean
+    fnameValid: Boolean,
+    lnameValid: Boolean,
+    emailValid: Boolean,
+    formValid: Boolean
 }
 
-let emailRegExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//eslint-disable-next-line
+let emailRegExp: RegExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 class ContactForm extends React.Component<formProps, formState> {
 
@@ -29,13 +33,20 @@ class ContactForm extends React.Component<formProps, formState> {
             email: '',
             message: ''
         },
-        isValid: true
+        fnameValid: true,
+        lnameValid: true,
+        emailValid: true,
+        formValid: false
     }
 
     constructor(formProps: any) {
         super(formProps);
         this.handleChange = this.handleChange.bind(this);
-        this.validate = this.validate.bind(this);
+        this.handleFname = this.handleFname.bind(this);
+        this.handleLname = this.handleLname.bind(this);
+        this.handleEmail = this.handleEmail.bind(this);
+        this.validateName = this.validateName.bind(this);
+        this.validateEmail = this.validateEmail.bind(this);
     }
 
     handleChange(event: any) {
@@ -45,32 +56,65 @@ class ContactForm extends React.Component<formProps, formState> {
             ...this.state.form,
             [name]: value
         }});
-        this.validate() ? this.setState({isValid: true}) : this.setState({isValid: false});
     }
 
-    validate(): boolean {
+    handleFname(event: any) {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({form: {
+            ...this.state.form,
+            [name]: value
+        }});
+        this.validateName(value) ? this.setState({fnameValid: true}) : this.setState({fnameValid: false});
+    }
+
+    handleLname(event: any) {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({form: {
+            ...this.state.form,
+            [name]: value
+        }});
+        this.validateName(value) ? this.setState({lnameValid: true}) : this.setState({lnameValid: false});
+    }
+
+    handleEmail(event: any) {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({form: {
+            ...this.state.form,
+            [name]: value
+        }});
+        this.validateEmail() ? this.setState({emailValid: true}) : this.setState({emailValid: false});
+    }
+
+    validateName(s: string): boolean {
+        return s.length > 0 ? true : false;
+    }
+
+    validateEmail(): boolean {
         return emailRegExp.test(this.state.form.email)
     }
 
     sendResults = () => {
-        if (this.validate()) {
-                this.setState({isValid: true});
-                formApi.post('/form-results',
-                {
-                    first_name: this.state.form.first_name,
-                    last_name: this.state.form.last_name,
-                    title: this.state.form.title,
-                    email: this.state.form.email,
-                    message: this.state.form.message
-                })
-                .then((res) => {
-                    alert('Submitted Succesfully!');
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        } else {
-            this.setState({isValid: false});
+        
+        if (this.state.fnameValid
+            || this.state.lnameValid
+            || this.state.emailValid) {
+            formApi.post('/form-results',
+            {
+                first_name: this.state.form.first_name,
+                last_name: this.state.form.last_name,
+                title: this.state.form.title,
+                email: this.state.form.email,
+                message: this.state.form.message
+            })
+            .then((res) => {
+                alert('Submitted Succesfully!');
+            })
+            .catch((err) => {
+                console.log(err);
+            });
         }
     }
 
@@ -80,8 +124,16 @@ class ContactForm extends React.Component<formProps, formState> {
                 <h2 id="contact-heading">Heading Two</h2>
                 <form className="form-content">
                     <div className="form-row">
-                        <input className="row-item" type="text" name="first_name" placeholder="First Name" value={this.state.form.first_name} onChange={ this.handleChange }/>
-                        <input className="row-item" type="text" name="last_name" placeholder="Last Name" value={this.state.form.last_name} onChange={ this.handleChange }/>
+                        <input className="row-item" type="text" name="first_name" style={ this.state.fnameValid ? {} : {border: '2px solid var(--mwi-red)'} } placeholder="First Name" value={this.state.form.first_name} onChange={ this.handleFname }/>
+                        {
+                            this.state.fnameValid === true ? null :
+                            <label id="required-fname-text">Required</label>
+                        }
+                        <input className="row-item" type="text" name="last_name" style={ this.state.lnameValid ? {} : {border: '2px solid var(--mwi-red)'} } placeholder="Last Name" value={this.state.form.last_name} onChange={ this.handleLname }/>
+                        {
+                            this.state.lnameValid === true ? null :
+                            <label id="required-text-right">Required</label>
+                        }
                     </div>
                     <div className="form-row">
                         <input className="row-item" type="text" name="title" placeholder="Title" value={this.state.form.title} onChange={ this.handleChange }/>
@@ -91,13 +143,13 @@ class ContactForm extends React.Component<formProps, formState> {
                             className="row-item"
                             type="email"
                             name="email"
-                            style={ this.state.isValid ? {} : {border: '2px solid var(--mwi-red)'} }
+                            style={ this.state.emailValid ? {} : {border: '2px solid var(--mwi-red)'} }
                             placeholder="Email"
                             value={ this.state.form.email }
-                            onChange={ this.handleChange }/>
+                            onChange={ this.handleEmail }/>
                         {
-                            this.state.isValid === true ? null :
-                            <p id="required-text">Required</p>
+                            this.state.emailValid === true ? null :
+                            <label id="required-text-right">Required</label>
                         }
                     </div>
                     <div className="form-row">
